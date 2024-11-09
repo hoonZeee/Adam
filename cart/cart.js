64641de10a82dcc17a0dbec8b539111fb8561e0c
kwonjjinho <109893466+kwonjjinho@users.noreply.github.com>
2024-11-10 01:32:44 +0900

    Add files via upload

--- cart/cart.js
+++ cart/cart.js
@@ -43,9 +43,9 @@ $(document).ready(function(){
           el.parent().parent().remove(); 
           if($(".product").length == 0) {
             if(check) {
-              $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
+              $("#cart").html("<h1>!</h1><p>여러 상품들을 둘러보고 장바구니에 담아주세요</a>. 감사합니다!</p>");
             } else {
-              $("#cart").html("<h1>No products!</h1>");
+              $("#cart").html("<h1>상품이 없습니다.!</h1>");
             }
           }
           changeTotal(); 
@@ -83,3 +83,98 @@ $(document).ready(function(){
     $(".remove").click();
   });
 });
+
+/* 여기는 개월 붙여서 렌트시에사용할 js
+function changeVal(el) {
+  // "개"를 분리하고 숫자로 변환
+  var qt = parseFloat(el.parent().children(".qt").html().replace("개", "").trim());
+  var price = parseFloat(el.parent().children(".price").html());
+  var eq = Math.round(price * qt * 100) / 100;
+  
+  el.parent().children(".full-price").html(eq + "만원");
+  
+  changeTotal();			
+}
+
+function changeTotal() {
+  var price = 0;
+  
+  $(".full-price").each(function(index){
+    // "만원" 제거 후 숫자로 변환
+    var priceText = $(".full-price").eq(index).html().replace("만원", "").trim();
+    price += parseFloat(priceText);
+  });
+  
+  price = Math.round(price * 100) / 100;
+  var tax = Math.round(price * 0.05 * 100) / 100;
+  var shipping = parseFloat($(".shipping span").html());
+  var fullPrice = Math.round((price + tax + shipping) * 100) / 100;
+  
+  if(price === 0) {
+    fullPrice = 0;
+  }
+  
+  $(".subtotal span").html(price);
+  $(".tax span").html(tax);
+  $(".total span").html(fullPrice);
+}
+
+$(document).ready(function(){
+  $(".remove").click(function(){
+    var el = $(this);
+    el.parent().parent().addClass("removed");
+    window.setTimeout(
+      function(){
+        el.parent().parent().slideUp('fast', function() { 
+          el.parent().parent().remove(); 
+          if($(".product").length === 0) {
+            if(check) {
+              $("#cart").html("<h1>The shop does not function, yet!</h1><p>If you liked my shopping cart, please take a second and heart this Pen on <a href='https://codepen.io/ziga-miklic/pen/xhpob'>CodePen</a>. Thank you!</p>");
+            } else {
+              $("#cart").html("<h1>No products!</h1>");
+            }
+          }
+          changeTotal(); 
+        });
+      }, 200);
+  });
+  
+  $(".qt-plus").click(function(){
+    var qtEl = $(this).parent().children(".qt");
+    var currentQty = parseInt(qtEl.html().replace("개", "").trim());
+    qtEl.html((currentQty + 1) + "개");
+    
+    $(this).parent().children(".full-price").addClass("added");
+    
+    var el = $(this);
+    window.setTimeout(function(){
+      el.parent().children(".full-price").removeClass("added"); 
+      changeVal(el);
+    }, 150);
+  });
+  
+  $(".qt-minus").click(function(){
+    var qtEl = $(this).parent().children(".qt");
+    var currentQty = parseInt(qtEl.html().replace("개", "").trim());
+    
+    if(currentQty > 1) {
+      qtEl.html((currentQty - 1) + "개");
+    }
+    
+    $(this).parent().children(".full-price").addClass("minused");
+    
+    var el = $(this);
+    window.setTimeout(function(){
+      el.parent().children(".full-price").removeClass("minused"); 
+      changeVal(el);
+    }, 150);
+  });
+  
+  window.setTimeout(function(){$(".is-open").removeClass("is-open")}, 1200);
+  
+  $(".btn").click(function(){
+    check = true;
+    $(".remove").click();
+  });
+});
+*/
